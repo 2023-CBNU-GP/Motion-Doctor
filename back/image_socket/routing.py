c423b0fa65f2746ca5c94e942356c83c7f58910b
choheejin <hjcho9510@gmail.com>
2023-05-25 15:29:08 +0900

    back: socket 임포트 에러 해결

--- back/image_socket/routing.py
+++ back/image_socket/routing.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import consumers
+from . import ImageConsumers
 
 # 웹소켓 요청이 들어오면 consumers로 연결
 websocket_urlpatterns = [
-    path('ws/socket_server', consumers.Consumers.as_asgi())
+    path('ws/socket_server', ImageConsumers.Consumers.as_asgi())
 ]
