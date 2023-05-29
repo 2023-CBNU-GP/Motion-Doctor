04311cbe69448608ebf2ac927c2a326127306184
choheejin <hjcho9510@gmail.com>
2023-05-29 17:46:34 +0900

    front: 네비게이션 바 이동 경로 추가

--- front/src/components/navigation.tsx
+++ front/src/components/navigation.tsx
@@ -23,14 +23,27 @@ export default function Navigation() {
     return (
         <div
             className="px-[120px] py-8 w-full h-10 bg-white drop-shadow sticky top-0 text-center items-center flex flex justify-between">
-            <div>
+            <div className="flex items-center gap-16">
                 <Link href="/" className="text-lg font-bold flex gap-2">
                     <div className="text-color-primary-500">Motion</div>
                     <div>Doctor</div>
                 </Link>
+                {
+                    logInfo?.type === "doctor" &&
+                    <div className="flex gap-6 text-sm">
+                        <Link className="hover:text-color-primary-500" href="/doctor/manage">내가 등록한 재활 코스</Link>
+                        <Link className="hover:text-color-primary-500" href="/test">재활코스 구경하기</Link>
+                    </div>
+                }
+                {
+                    logInfo?.type === "patient" &&
+                    <div className="flex gap-6 text-sm">
+                        <Link className="hover:text-color-primary-500" href="/patient/postures">내가 등록한 재활 코스</Link>
+                        <Link className="hover:text-color-primary-500" href="/test">재활코스 구경하기</Link>
+                    </div>
 
+                }
             </div>
-
             <div>
                 {
                     logInfo ? <div className="flex gap-3">
