3ac9e810d995d8677ec4856b2b93a81c130e03dd
songsuyoung <tndud3999@naver.com>
2023-09-15 22:03:35 +0900

    의사 오류 수정

--- back/api/FileViews.py
+++ back/api/FileViews.py
@@ -81,7 +81,7 @@ class FileUpload(APIView):
 
                 if Curframe >= frameCount / 3 and Curframe <= frameCount - frameCount / 3:  # 현재 프레임 수를 확인 후, 지정된 프레임 이상일 시 동영상에서 스켈렙톤 뽑아내기
                     img = detector.findPose(img)
-                    lmList = detector.findPosition(img)
+                    lmList,_ = detector.findPosition(img)
                     # 사이각 구하기 공식
                     angleManager.GetAngle(lmList, DoctorAngle)
                     angleManager.GetAverageAngle(lmList, DoctorAngle)
