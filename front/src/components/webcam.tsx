ff4726c21d1784c30adae8fb60fd21d39f6a885d
choheejin <hjcho9510@gmail.com>
2023-09-18 16:56:36 +0900

    front: 웹캠 촬영시 에러 해결

--- front/src/components/webcam.tsx
+++ front/src/components/webcam.tsx
@@ -7,7 +7,7 @@ const VIDEO_URL = process.env.NEXT_PUBLIC_SOCKET + '/ws/webcam';
 
 export default function WebCam({typeData, name}: { typeData: string, name: string }) {
     const webcamRef = useRef<any>(null);
-    const mediaRecorderRef = useRef<any>(null);
+    const mediaRecorderRef = useRef<any>();
     const interval = useRef<any>();
 
     const socketObject = useRef<any>();
@@ -133,7 +133,11 @@ export default function WebCam({typeData, name}: { typeData: string, name: strin
                         </svg>
                     </div>
                 ) : (
-                    <div onClick={handleStartCaptureClick} className="cursor-pointer">
+                    <div onClick={() => {
+                        if (webcamRef.current.stream != null) {
+                            handleStartCaptureClick()
+                        }
+                    }} className="cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="w-8 h-8">
                             <path fillRule="evenodd"
                                   d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
